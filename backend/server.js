628f3da5f119548b4a120c3160d81a65972d2ece
eisha-stack <eishaofficial13@gmail.com>
2025-10-02 21:49:01 +0530

    created user authentication in the backend

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,7 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
-import dotenv from "dotenv";
-dotenv.config();
+import 'dotenv/config'
 
 
 // app config
