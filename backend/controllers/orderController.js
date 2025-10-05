28949a1d15c553adef5f78ec4c23057023ebd7b4
eisha-stack <eishaofficial13@gmail.com>
2025-10-06 02:04:07 +0530

    creating place order feature

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,12 @@
+import orderModel from "../models/orderModels.js";
+import userModel from "../models/userModel.js"
+import stripe from "stripe"
+
+
+//placing user order from frontend
+
+const placeOrder = async (req,res) =>{
+
+}
+
+export {placeOrder}
