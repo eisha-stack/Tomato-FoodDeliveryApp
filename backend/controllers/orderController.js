d35199aabfb2c71cd170975f77394e8609f59340
eisha-stack <eishaofficial13@gmail.com>
2025-10-07 23:47:30 +0530

    Integrating Stripe Payment Gateway

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,12 +1,46 @@
 import orderModel from "../models/orderModels.js";
 import userModel from "../models/userModel.js"
-import stripe from "stripe"
+import Stripe from "stripe"
 
 
 //placing user order from frontend
 
 const placeOrder = async (req,res) =>{
 
+    try {
+        const newOrder = newModel({
+            userId:req.body.userId,
+            item:req.body.items,
+            amount:req.body.amount,
+            address:req.body.address
+        })
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
+
+        const line_items = req.body.items.map((item)=>({
+            price_data:{
+                currency:"inr",
+                product_data:{
+                    name:item.name
+                },
+                unit_amount:item.price*100
+            },
+            qunatity:item.quantity
+        }))
+
+        line_items.push({
+            price_data:{
+                currency:"inr",
+                product_data:{
+                    name:"Delivery Charges"
+                },
+                unit_amount:2*100
+            },
+            quantity:1
+        })
+    } catch (error) {
+        
+    }
 }
 
 export {placeOrder}
